fa28820a0d8e970af2fec36079da27af9f539a1d
eyvmal <eyvind.malde@gmail.com>
2024-09-24 10:48:21 +0200

    core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
+function addCard() {
+    const cards = document.querySelector(".cards")
 
-console.log(data);
+    data.forEach((card) => {
+        const li = document.createElement('li')
+        li.className = 'card'
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+        const h2 = document.createElement('h2')
+        h2.className = 'card--title'
+        h2.textContent = card.name.charAt(0).toUpperCase() + card.name.slice(1);
+        li.appendChild(h2)
+
+        const img = document.createElement('img')
+        img.className = 'card--img'
+        img.width = 256
+        img.src = card.sprites.other['official-artwork'].front_default;
+        li.appendChild(img);
+
+        const ul = document.createElement('ul');
+        ul.className = 'card--text';
+
+        card.stats.forEach((stat) => {
+            const statLi = document.createElement('li');
+            statLi.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`;
+            ul.appendChild(statLi);
+        });
+
+        li.appendChild(ul);
+        cards.appendChild(li);
+    })
+}
+
+addCard();
